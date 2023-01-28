ad1a1e633039202cb196041b46c6569023b42eb7
kaicx1 <KCXUE@cpp.edu>
2023-01-27 18:46:48 -0800

    Adding Comments
    added comments

--- reddetection.cpp
+++ reddetection.cpp
@@ -7,21 +7,19 @@ using namespace std;
 int main( int argc, const char** argv ){
      VideoCapture cap(0);
      Mat myCam, myCam_hsv, redMask;
-
-     if(!cap.isOpened()){
+     if(!cap.isOpened()){ // If no camera return -1 
           return -1;
      }
-     
      while(1){
-          cap >> myCam; // get a new frame from camera
-          cvtColor(myCam, myCam_hsv, COLOR_BGR2HSV);
-          Vec3b L_limit(160,100,20);
-          Vec3b U_limit(179,255,255);
+          cap >> myCam; // Open Camera
+          cvtColor(myCam, myCam_hsv, COLOR_BGR2HSV); // camera to HSV
+          Vec3b L_limit(160,100,20); // Setting lower limit
+          Vec3b U_limit(179,255,255); // Setting upper limit
           inRange(myCam_hsv, L_limit, U_limit, redMask);
           Mat red;
           bitwise_and(myCam, myCam, red, redMask);
-          imshow("Original", myCam);
-          imshow("Red", red);
+          imshow("Original", myCam); //  show original camera 
+          imshow("Red", red); // show red only camera
 
           if(waitKey(1) == 27){
                break;
