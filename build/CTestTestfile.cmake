adf020c581f9ffda1e006a66d56c04f1715c3967
20Lush <zachfsx83@gmail.com>
2023-01-29 14:15:51 -0800

    adjusting git ignore

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/.NGCP/fire-detection/OpenCVDetectRed
-# Build directory: D:/.NGCP/fire-detection/OpenCVDetectRed/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
